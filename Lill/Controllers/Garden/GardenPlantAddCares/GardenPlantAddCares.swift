3432611d5a8419f38991a479be15bb4851f932ca
Alex <alex.terode@gmail.com>
2021-12-08 23:07:55 +0200

    Add custom cares scroll, prepare logic

--- Lill/Controllers/Garden/GardenPlantAddCares/GardenPlantAddCares.swift
+++ Lill/Controllers/Garden/GardenPlantAddCares/GardenPlantAddCares.swift
@@ -3,7 +3,23 @@ import UIKit
 
 class GardenPlantAddCares: BaseController {
     
+    //----------------------------------------------
+    // MARK: - IBOutlet
+    //----------------------------------------------
+    
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var caresViews: [GardenPlantCareView]!
+    
+    //----------------------------------------------
+    // MARK: - Private property
+    //----------------------------------------------
+    
+    private var cares: [CareType] = []
+    private var selectedCares = Set<CareType>()
+    
+    //----------------------------------------------
+    // MARK: - Life cycle
+    //----------------------------------------------
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +37,9 @@ class GardenPlantAddCares: BaseController {
         rightBarButtonItem.setTitleTextAttributes([NSAttributedString.Key.font : UIFont(name: "SFProDisplay-Regular", size: 17.0)!, NSAttributedString.Key.foregroundColor : UIColor(rgb: 0x7CDAA3)], for: .normal)
 //        leftBarButtonItem.setTitleTextAttributes([NSAttributedString.Key.font : UIFont(name: "SFProDisplay-Regular", size: 17.0)!, NSAttributedString.Key.foregroundColor : UIColor(rgb: 0x7CDAA3)], for: .normal)
         navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        scrollView.contentInset.left = 9.0
+        scrollView.contentInset.right = 9.0
     }
     
     //----------------------------------------------
@@ -34,4 +53,24 @@ class GardenPlantAddCares: BaseController {
     @objc func backAction() {
         navigationController?.popViewController(animated: true)
     }
+}
+
+//----------------------------------------------
+// MARK: - AddPlantCareProtocol
+//----------------------------------------------
+
+extension GardenPlantAddCares: GardenPlantCareViewProtocol {
+    func didSelectCare(view: GardenPlantCareView, selectedType: CareType) {
+        
+    }
+    
+//    func addPlantCareSelect(view: AddPlantCareView, selectedType: CareType) {
+//        if selectedCares.contains(selectedType) {
+//            selectedCares.remove(selectedType)
+//        } else {
+//            selectedCares.insert(selectedType)
+//        }
+//
+//        changesView()
+//    }
 }
