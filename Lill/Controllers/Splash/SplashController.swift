a93629b9ccd8fedc8791f99023922f07a01b908b
Andrey.S <andrey.s@mail.int>
2021-12-15 12:17:53 +0200

    Build for TestFlight v.1.0(25)

--- Lill/Controllers/Splash/SplashController.swift
+++ Lill/Controllers/Splash/SplashController.swift
@@ -3,12 +3,16 @@ import UIKit
 
 class SplashController: BaseController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
     //----------------------------------------------
     // MARK: - Life cycle
     //----------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        infoLabel.text = RLocalization.login_info.localized(PreferencesManager.sharedManager.languageCode.rawValue)
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             if let _ = KeychainService.standard.newAuthToken {
                 RootRouter.sharedInstance.loadPlants(toWindow: RootRouter.sharedInstance.window!)
